087411b40872e448a65da8874acddd9b40e65f96
MD Minarul Islam <wdminarul>
2024-08-07 11:41:26 +0600

    constional-return

---
+++ conditional_return.js
@@ -0,0 +1,12 @@
+// return use of condition
+function eveenNumber (number){
+    if(number % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(eveenNumber(10))
+console.log(eveenNumber(7));
