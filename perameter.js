7a16284c863051616b886b94a9d1a9992f7deb6d
MD Minarul Islam <wdminarul>
2024-08-07 11:01:19 +0600

    perameter

---
+++ perameter.js
@@ -0,0 +1,23 @@
+
+//function perameter 
+function squre(number){
+    let squreNum = number * number;
+    // return squreNum
+//    return console.log(squreNum);
+}
+
+squre(50);
+
+// multiple number squre show 
+
+function multiSqure (num,num1,num2){
+    let squre1 =num * num;
+    let squre2 = num1 * num1;
+    let squre3 = num2 * num2;
+
+    console.log('This is squre of the value number perameter 1: ',squre1 );
+    console.log('This is squre of the value number perameter 2: ',squre2 );
+    console.log('This is squre of the value number perameter 3: ',squre3 );
+}
+
+multiSqure(4,56,8);
