a5c9f8539236a59efedd41812a5424dfcbcf5cd7
MD Minarul Islam <wdminarul>
2024-08-07 11:14:45 +0600

    arguments.js

---
+++ arguments.js
@@ -0,0 +1,7 @@
+function ageDifference(num1,num2){
+    let different = num1 - num2;
+    console.log('mother age :',num1,",", 'Child age:', num2)
+    console.log('Age diference is :',different);
+}
+
+ageDifference(49,23);; // this call function arguments
